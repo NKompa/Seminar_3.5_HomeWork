e08626b26258118d153555b89e0beb70b0d39718
NKompa <nkompa7@gmail.com>
2022-09-05 15:40:58 +0300

    Count even numbers in array

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] array = new int [4];
+int countEven = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,999);
+    if(array[i]%2 == 0)
+    {
+        countEven++;
+    }
+}
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine("Количество чётных чисел: "+ countEven);
