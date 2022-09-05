87111769696c56d70d02e2062b5312cac3cb00c8
NKompa <nkompa7@gmail.com>
2022-09-05 22:43:41 +0300

    Array max & min difference

---
+++ Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. [3 7 22 2 78] -> 76
+
+int [] array = new int [5];
+int Max = 0;
+int Min = 100;
+int Diff = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,100);
+    if(array[i]>Max)
+    {
+        Max = array[i];
+    }
+    else if(array[i]<Min)
+    {
+        Min = array[i];
+    }
+}
+Diff = Max - Min;
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine(Diff);
